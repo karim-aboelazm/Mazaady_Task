10bdee49e03b65a659484a0b5f060455bdc9c0e7
karim-aboelazm <20912017100480@fci.zu.edu.eg>
2025-02-09 16:16:17 +0200

    Use Confirm Order Function Instead of Computing Function

--- sales_customization/models/sales_customization.py
+++ sales_customization/models/sales_customization.py
@@ -10,11 +10,13 @@ class SalesOrderCustomization(models.Model):
     )
     total_amount_after_discount = fields.Float(
         string=_('Total Amount After Discount'),
-        compute='_get_total_amount_after_discount',
-        store=True,
     )
 
-    @api.depends('amount_total', 'discount_amount')
     def _get_total_amount_after_discount(self):
         for order in self:
             order.total_amount_after_discount = order.amount_total - order.discount_amount
+
+    def action_confirm(self):
+        res = super(SalesOrderCustomization, self).action_confirm()
+        self._get_total_amount_after_discount()
+        return res
